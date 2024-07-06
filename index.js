65dd5d1d917381912a137058a9019ae299b121cb
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-07-06 20:47:16 +0530

    add

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 
 // middileware configuration
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:"http://localhost:5173"}));
 app.use(express.static("public"));
 
 
