24153e80c6a4eaa59d6c0a31e17f638b0abc9a70
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-07-07 09:21:15 +0530

    add

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotEnv.config();
 
 // middileware configuration
 // Use CORS middleware to allow requests 
-app.use(cors());
+app.use(cors({origin :"http://localhost:5173"}));
 app.use(express.json());
 app.use(express.static("public"));
 
